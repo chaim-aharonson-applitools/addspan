70b13b6b90ff7748154e728294769bdf01457791
Noam Davidovitz <noam.davidovitz@applitools.com>
2022-05-08 19:39:16 +0300

    Improve text nodes split - node.data.split(/(\s+)/). We can handle many kind of spaces.

--- index.js
+++ index.js
@@ -27,49 +27,21 @@ textNodes.forEach(node =>
 {
   if(node.data)
   {  
-    const txts = node.data.split(" ");
+    const splitedText = node.data.split(/(\s+)/); 
     const parent = node.parentNode;
-    const wrapper = document.createElement("span");
-    const space1 = String.fromCharCode(160);
-    const space2 = "\u2009";
-    const t = node.data[0];
-    let statrtWithSpace = t.includes(space1) || t.includes(" ") || t.includes(space2);
-    
-    txts.forEach((txt, idx) => 
-    {
-      const trimText = txt.trim();
-      
-      const wrappText = trimText.length > 0;
-      if(wrappText)
-      {
-        if (idx == 0 && statrtWithSpace)
-        {
-          wrapper.appendChild(document.createTextNode(node.data[0]));
-        }
-        
-        wrapper.appendChild(document.createTextNode(trimText));
-        if(idx<txts.length-1)
-        {
-          wrapper.appendChild(document.createTextNode(" "));
-        }
-      }
-      else
-      {
-        wrapper.appendChild(document.createTextNode(" "));
-      }
-    });
-    
-    const splitedTextNodes = Array.from(wrapper.childNodes);
     const origNodes = Array.from(parent.childNodes);
-    var updatedNodes = [];
     
+    var updatedNodes = [];
     origNodes.forEach(child => 
     {
       if (child == node)
       {
-        splitedTextNodes.forEach(txtNode =>
+        splitedText.forEach(txt =>
           {
-              updatedNodes.push(txtNode);
+            if (txt.length > 0)
+            {
+              updatedNodes.push(document.createTextNode(txt));
+            }
           });
       }
       else

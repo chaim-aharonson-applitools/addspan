c5423ffd0ce4acbdbff26ed7ad65d6a857f9d403
Chaim Aharonson <chaim.aharonson@applitools.com>
2022-06-30 12:55:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,65 @@
+
 //https://coryrylan.com/blog/wrapping-dom-text-nodes-with-javascript
 //javascript:
-const selector = 'body';
+const selector = '.Layout-main';
 const elt = document.querySelector(selector);
+const layer = document.createElement('div');
+layer.style.cssText = 'position:fixed;left:0;top:0;bottom:0;right:0;z-index:22222;background-color: rgba(255,255,255,0.5)';
+document.body.appendChild(layer);
 let textNodes = [];
 const getNodes = (parent) => {
   const children = Array.from(parent.childNodes);
-  if (children && children.length){
+  if (children && children.length) {
     children.forEach((node) => {
       if (node.nodeType === 3 && node.textContent.trim().length > 1) {
         textNodes.push(node);
-      } else {
+      }
+      else {
         getNodes(node);
       }
     });
   }
 };
+
+
+const range = document.createRange();
+let textAndLocList = [];
 getNodes(elt);
 textNodes.forEach(node => {
-  if(node.data){
-    const txts = node.data.split(" ");
-    const parent = node.parentNode;
-    const wrapper = document.createElement("span");
-    txts.forEach((txt, idx) => {
-      if(txt.trim().length){
-        const span = document.createElement("span");
-        span.innerText = txt.trim();
-        wrapper.appendChild(span);
-        if(idx<txts.length-1){
-          wrapper.appendChild(document.createTextNode(" "));
-        };
-      }else{
-        wrapper.appendChild(document.createTextNode(" "));
+  if (node.data) {
+    const splitedText = node.data.split(/(\s+)/);
+    const splitedTextNodes = [];
+    splitedText.forEach(txt => {
+      if (txt.length > 0) {
+        splitedTextNodes.push(document.createTextNode(txt));
       }
     });
-    parent.replaceChild(wrapper, node);
-  };
+
+    const parent = node.parentNode;
+    const origNodes = Array.from(parent.childNodes);
+
+    var updatedNodes = [];
+    origNodes.forEach(child => {
+      if (child == node) {
+        splitedTextNodes.forEach(txtNode => { updatedNodes.push(txtNode); });
+      }
+      else {
+        updatedNodes.push(child);
+      }
+    });
+
+    origNodes.forEach(child => { parent.removeChild(child); });
+    updatedNodes.forEach(child => { parent.appendChild(child); });
+
+    splitedTextNodes.forEach(child => {
+      range.selectNode(child);
+      const rect = range.getBoundingClientRect();
+      const rect_layer = document.createElement('div');
+      rect_layer.style.cssText = `position:absolute;left:${rect.x}px;top:${rect.y}px;width:${rect.width}px;height:${rect.height}px;z-index:10;color: yellow;`;
+      rect_layer.innerHTML = child.data;
+      layer.appendChild(rect_layer);
+      const textAndLoc = { text: child.data, rect };
+      textAndLocList.push(textAndLoc);
+    });
+  }
 })
